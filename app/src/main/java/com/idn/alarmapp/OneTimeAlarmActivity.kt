028e3ceabb51e4459c89be646f8fb8b86ac16906
ZakiHidayat <zakihidayat260@gmail.com>
2022-01-17 15:07:46 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/alarmapp/OneTimeAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.idn.alarmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OneTimeAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_time_alarm)
+    }
+}
