764b7092487cdd1705ac661d6e07dd77e514df51
ZakiHidayat <zakihidayat260@gmail.com>
2022-04-13 09:45:19 +0700

    baru

--- app/src/main/java/com/idn/alarmapp/MainActivity.kt
+++ app/src/main/java/com/idn/alarmapp/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.idn.alarmapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAlarmType()
+        initTimeToday()
+        initDateToday()
+//        initRecyclerView()
+    }
+
+//    private fun initRecyclerView() {
+//        alarmAdapter = AlarmAdapter()
+//        rv_reminder_alarm.apply {
+//            layoutManager = LinearLayoutManager(applicationContext)
+//            adapter = alarmAdapter
+//            swipeToDelete(this)
+//        }
+//    }
+//
+//    private fun swipeToDelete(recyclerView: RecyclerView) {
+//        ItemTouchHelper(object :
+//            ItemTouchHelper.SimpleCallback(
+//                0,
+//                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+//            ) {
+//            override fun onMove(
+//                recyclerView: RecyclerView,
+//                viewHolder: RecyclerView.ViewHolder,
+//                target: RecyclerView.ViewHolder
+//            ): Boolean {
+//                return true
+//            }
+//
+//            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//                // TODO Cancel alarm by type when item of RecyclerView onSwiped()
+//                val typeOfAlarm = alarmAdapter.alarms[viewHolder.adapterPosition].type
+//                alarmReceiver.cancelAlarm(this@MainActivity, typeOfAlarm)
+//
+//                val deletedItem = alarmAdapter.alarms[viewHolder.adapterPosition]
+//                // delete item
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    db.alarmDao().deleteAlarm(deletedItem)
+//                }
+//                alarmAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+//
+//            }
+//        }).attachToRecyclerView(recyclerView)
+//    }
+
+    private fun initAlarmType() {
+        view_set_one_time_alarm.setOnClickListener {
+            startActivity(Intent(this, OneTimeAlarmActivity::class.java))
+        }
+
+        view_set_repeating_alarm.setOnClickListener {
+            startActivity(Intent(this, RepeatingAlarmActivity::class.java))
+        }
+    }
+
+    private fun initDateToday() {
+   // E = hari dd = tanggal MM = Bulan yyyy = Tahun
+        val dateNow: Date = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("E, dd MMM yyyy", Locale.getDefault())
+        val formattedDate: String = dateFormat.format(dateNow)
+        tv_date_today.text = formattedDate
+    }
+
+    private fun initTimeToday() {
+   // HH = hours mm = minute
+        val timeNow = Calendar.getInstance()
+        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val formattedTime = timeFormat.format(timeNow.time)
+        tv_time_today.text = formattedTime
     }
 }
--- app/src/main/java/com/idn/alarmapp/OneTimeAlarmActivity.kt
+++ app/src/main/java/com/idn/alarmapp/OneTimeAlarmActivity.kt
@@ -2,10 +2,53 @@ package com.idn.alarmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.idn.alarmapp.databinding.ActivityMainBinding
+import com.idn.alarmapp.fragment.DatePickerFragment
+import com.idn.alarmapp.fragment.TimePickerFragment
+import kotlinx.android.synthetic.main.activity_one_time_alarm.*
 
-class OneTimeAlarmActivity : AppCompatActivity() {
+class OneTimeAlarmActivity : AppCompatActivity() , View.OnClickListener, DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener{
+
+//    private var binding: ActivityMainBinding? = null
+//    private lateinit var alarmReceiver: AlarmReceiver
+//    private val db by lazy {AlarmDB (this)}
+
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_time_alarm)
+
+        btn_set_date_one_time.setOnClickListener(this)
+        btn_set_time_one_time.setOnClickListener(this)
+//        btn_add_set_one_time_alarm.setOnClickListener(this)
+//        btn_cancel_set_one_time_alarm.setOnClickListener(this)
+//
+//        alarmReceiver = AlarmReceiver()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_set_date_one_time -> {
+                val datePickerFragment = DatePickerFragment()
+                datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
+            }
+            R.id.btn_set_time_one_time -> {
+                val timePickerFragmentOneTime = TimePickerFragment()
+                timePickerFragmentOneTime.show(supportFragmentManager, TIME_PICKER_ONCE_TAG)
+            }
+    }
+   }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int) {
+        TODO("Not yet implemented")
     }
 }
